4b9e5795b29836d6a2c59f574c51c2e0a4d7bfe5
agit oktay <agitoktay@gmail.com>
2020-10-20 14:16:00 +0300

    NDU-346

--- lib/widgets/dash_widget_helper.dart
+++ lib/widgets/dash_widget_helper.dart
@@ -1,7 +1,6 @@
-
-
 import 'package:ndu_dashboard_widgets/models/widget_config.dart';
 import 'package:ndu_dashboard_widgets/widgets/cards/ndu_widget_cards_simple_card.dart';
+import 'package:ndu_dashboard_widgets/widgets/charts/ndu_widget_charts_timeseries_bars_flot.dart';
 import 'package:ndu_dashboard_widgets/widgets/not_implemented_widget.dart';
 
 import 'base_dash_widget.dart';
@@ -15,6 +14,7 @@ class DashboardWidgetHelper {
       }
       if (widgetConfig.bundleAlias == "charts") {
         if (widgetConfig.typeAlias == "basic_timeseries") return BasicTimeseriesChart(widgetConfig);
+        if (widgetConfig.typeAlias == "timeseries_bars_flot") return TimeSeriesBarsFlot(widgetConfig);
       }
     }
     return NotImplementedWidget(widgetConfig);
