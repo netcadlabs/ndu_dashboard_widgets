3c30ec11ed6dd62ac6d493e6c9200cc630569dcf
eyup.akky <2255629e>
2021-01-19 15:26:19 +0300

    http://jira.netcad.com.tr/browse/NDU-484

--- lib/widgets/timeago/time_ago.dart
+++ lib/widgets/timeago/time_ago.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:timeago/timeago.dart' as timeago;
+import 'package:timeago/timeago.dart' as timeAgo;
 
 class TimeAgo extends StatelessWidget {
-  Locale myLocale;
-  DateTime lastTime;
-
+  final DateTime lastTime;
   TimeAgo(this.lastTime);
 
   @override
   Widget build(BuildContext context) {
-    myLocale = Localizations.localeOf(context);
-    if (myLocale.languageCode == 'tr') timeago.setLocaleMessages(myLocale.languageCode, timeago.TrMessages());
-    else timeago.setLocaleMessages(myLocale.languageCode, timeago.EnMessages());
+    final Locale myLocale= Localizations.localeOf(context);
+    if (myLocale.languageCode == 'tr') timeAgo.setLocaleMessages(myLocale.languageCode, timeAgo.TrMessages());
+    else timeAgo.setLocaleMessages(myLocale.languageCode, timeAgo.EnMessages());
     return Container(
       alignment: Alignment.centerRight,
       margin: EdgeInsets.all(5),
       child: Text(
-        timeago.format(lastTime, locale: myLocale.languageCode),
+        timeAgo.format(lastTime, locale: myLocale.languageCode),
         style: Theme.of(context).textTheme.bodyText1,
       ),
     );
